b1122a5bd8586e8a9d4c893bf7fc1b62a16f2e50
Neal Terrell <nealdt@gmail.com>
2025-06-10 10:00:35 -0700

    Modern C++ style. SFML 3.0.

--- src/StbImage.cpp
+++ src/StbImage.cpp
@@ -4,14 +4,15 @@
 #include <string>
 #include <iostream>
 
-StbImage::StbImage() : m_width(0), m_height(0), m_bpp(0) {
+StbImage::StbImage() : m_width{ 0 }, m_height{ 0 }, m_bpp{ 0 } {
 }
 
 void StbImage::loadFromFile(const std::string& filepath) {
-    unsigned char* data = stbi_load(filepath.c_str(), &m_width, &m_height, &m_bpp, 4);
+    unsigned char* data{ stbi_load(filepath.c_str(), &m_width, &m_height, &m_bpp, 4) };
 
-    if (data == nullptr)
+    if (data == nullptr) {
         throw std::runtime_error("Could not load file " + filepath);
+    }
 
     m_data = std::unique_ptr<unsigned char[]>(data);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@ This application displays a textured mesh whose vertices also contain (u, v) tex
 */
 #include <glad/glad.h>
 #include <iostream>
-#include <memory>
 #include <filesystem>
 
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -32,7 +32,7 @@ struct Vertex3D {
 };
 
 ShaderProgram textureShader() {
-	ShaderProgram shader;
+	ShaderProgram shader{};
 	try {
 		shader.load("shaders/texture_perspective.vert", "shaders/texturing.frag");
 	}
@@ -45,7 +45,7 @@ ShaderProgram textureShader() {
 
 Mesh constructMesh(const std::vector<Vertex3D>& vertices, const std::vector<uint32_t>& faces) {
 	Mesh m{};
-	m.faces = faces.size();
+	m.faces = static_cast<uint32_t>(faces.size());
 
 	// Generate a vertex array object on the GPU.
 	glGenVertexArrays(1, &m.vao);
@@ -82,16 +82,17 @@ Mesh constructMesh(const std::vector<Vertex3D>& vertices, const std::vector<uint
 	return m;
 }
 
-const size_t FLOATS_PER_VERTEX = 3;
-const size_t VERTICES_PER_FACE = 3;
+const size_t VERTICES_PER_FACE{ 3 };
 
 // Reads the vertices and faces of an Assimp mesh, and uses them to initialize mesh structures
 // compatible with the rest of our application.
 void fromAssimpMesh(const aiMesh* mesh, std::vector<Vertex3D>& vertices,
 	std::vector<uint32_t>& faces) {
-	for (size_t i = 0; i < mesh->mNumVertices; i++) {
+	for (size_t i{ 0 }; i < mesh->mNumVertices; ++i) {
 		// Each "vertex" from Assimp has to be transformed into a Vertex3D in our application.
-		vertices.push_back({ mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z });
+		vertices.push_back(
+			Vertex3D{ mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z }
+		);
 		// TODO: construct the vertex above with texture coordinates (u, v) in addition to (x, y, z).
 		// To get (u, v) for the current Assimp vertex, access the mTextureCoords field of the
 		// "mesh" local variable. mTextureCoords[0] is the first texture of the mesh (in the future,
@@ -104,7 +105,7 @@ void fromAssimpMesh(const aiMesh* mesh, std::vector<Vertex3D>& vertices,
 	}
 
 	faces.reserve(mesh->mNumFaces * VERTICES_PER_FACE);
-	for (size_t i = 0; i < mesh->mNumFaces; i++) {
+	for (size_t i = 0; i < mesh->mNumFaces; ++i) {
 		// We assume the faces are triangular, so we push three face indexes at a time into our faces list.
 		faces.push_back(mesh->mFaces[i].mIndices[0]);
 		faces.push_back(mesh->mFaces[i].mIndices[1]);
@@ -115,13 +116,13 @@ void fromAssimpMesh(const aiMesh* mesh, std::vector<Vertex3D>& vertices,
 // Loads an asset file supported by Assimp, extracts the first mesh in the file, and fills in the 
 // given vertices and faces lists with its data.
 Mesh assimpLoad(const std::string& path, bool flipUvs = false) {
-	int flags = (aiPostProcessSteps)aiProcessPreset_TargetRealtime_MaxQuality;
+	int flags{ (aiPostProcessSteps)aiProcessPreset_TargetRealtime_MaxQuality };
 	if (flipUvs) {
 		flags |= aiProcess_FlipUVs;
 	}
 
-	Assimp::Importer importer;
-	const aiScene* scene = importer.ReadFile(path, flags);
+	Assimp::Importer importer{};
+	const aiScene* scene{ importer.ReadFile(path, flags) };
 
 	// If the import failed, report it
 	if (nullptr == scene) {
@@ -129,8 +130,8 @@ Mesh assimpLoad(const std::string& path, bool flipUvs = false) {
 		exit(1);
 	}
 	else {
-		std::vector<Vertex3D> vertices;
-		std::vector<uint32_t> faces;
+		std::vector<Vertex3D> vertices{};
+		std::vector<uint32_t> faces{};
 		fromAssimpMesh(scene->mMeshes[0], vertices, faces);
 		return constructMesh(vertices, faces);
 	}
@@ -173,17 +174,17 @@ uint32_t generateTexture(const StbImage& texture) {
 
 // A scene of a textured triangle.
 Mesh triangle() {
-	std::vector<Vertex3D> triangleVertices = {
+	std::vector<Vertex3D> triangleVertices{
 		{ -0.5, -0.5, 0, 0, 1 },
 		{ -0.5, 0.5, 0,  0, 0 },
 		{  0.5, 0.5, 0,  1, 0 }
 	};
-	std::vector<uint32_t> triangleFaces = {
+	std::vector<uint32_t> triangleFaces{
 		2, 1, 0
 	};
-	Mesh m = constructMesh(triangleVertices, triangleFaces);
+	Mesh m{ constructMesh(triangleVertices, triangleFaces) };
 
-	StbImage wall;
+	StbImage wall{};
 	wall.loadFromFile("models/wall.jpg");
 	m.texture = generateTexture(wall);
 	return m;
@@ -192,15 +193,15 @@ Mesh triangle() {
 Mesh bunny() {
 	// Load the bunny, but vertically flip its UV coordinates, because the creator uses 
 	// (0, 0) as the lower LEFT corner of texture space.
-	Mesh obj = assimpLoad("models/bunny_textured.obj", true);
-	StbImage texture;
+	Mesh obj{ assimpLoad("models/bunny_textured.obj", true) };
+	StbImage texture{};
 	texture.loadFromFile("models/bunny_textured.jpg");
 	obj.texture = generateTexture(texture);
 	return obj;
 }
 
 glm::mat4 buildModelMatrix(const glm::vec3& position, const glm::vec3& orientation, const glm::vec3& scale) {
-	auto m = glm::translate(glm::mat4(1), position);
+	auto m{ glm::translate(glm::mat4(1), position) };
 	m = glm::scale(m, scale);
 	m = glm::rotate(m, orientation[2], glm::vec3(0, 0, 1));
 	m = glm::rotate(m, orientation[0], glm::vec3(1, 0, 0));
@@ -209,57 +210,68 @@ glm::mat4 buildModelMatrix(const glm::vec3& position, const glm::vec3& orientati
 }
 
 int main() {
-	std::cout << std::filesystem::current_path() << std::endl;
 	// Initialize the window and OpenGL.
 	sf::ContextSettings settings;
 	settings.depthBits = 24; // Request a 24 bits depth buffer
 	settings.stencilBits = 8;  // Request a 8 bits stencil buffer
-	settings.antialiasingLevel = 2;  // Request 2 levels of antialiasing
-	settings.majorVersion = 3;
+	settings.majorVersion = 3; // You might have to change these on Mac.
 	settings.minorVersion = 3;
-	sf::Window window(sf::VideoMode{ 1200, 800 }, "Modern OpenGL", sf::Style::Resize | sf::Style::Close, settings);
+
+	sf::Window window{
+		sf::VideoMode::getFullscreenModes().at(0), "Modern OpenGL",
+		sf::Style::Resize | sf::Style::Close,
+		sf::State::Windowed, settings
+	};
 
 	gladLoadGL();
+	glEnable(GL_DEPTH_TEST);
 
 	// Inintialize scene objects.
-	Mesh obj = bunny();
+	Mesh obj{ bunny() };
 	//Mesh obj = triangle();
-	glm::vec3 objectPosition = { 0, 0, -3 };
-	glm::vec3 objectOrientation = { 0, 0, 0 };
-	glm::vec3 objectScale = { 3, 3, 3 };
+	glm::vec3 objectPosition{ 0, 0, -3 };
+	glm::vec3 objectOrientation{ 0, 0, 0 };
+	glm::vec3 objectScale{ 3, 3, 3 };
 
 	// Activate the shader program.
-	ShaderProgram program = textureShader();
+	ShaderProgram program{ textureShader() };
 	program.activate();
 
 	// Ready, set, go!
-	bool running = true;
+	bool running{ true };
 	sf::Clock c;
-
-	glEnable(GL_DEPTH_TEST);
 
 	auto last = c.getElapsedTime();
 	while (running) {
-		sf::Event ev;
-		while (window.pollEvent(ev)) {
-			if (ev.type == sf::Event::Closed) {
-				running = false;
+		// Check for events.
+		while (const std::optional event{ window.pollEvent() }) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
 			}
 		}
-
-		auto now = c.getElapsedTime();
-		auto diff = now - last;
-		std::cout << 1 / diff.asSeconds() << " FPS " << std::endl;
+		auto now{ c.getElapsedTime() };
+		auto diff{ now - last };
 		last = now;
 
+#ifdef LOG_FPS
+		// FPS calculation.
+		std::cout << 1 / diff.asSeconds() << " FPS " << std::endl;
+#endif
+
 		// Set up the view and projection matrices.
-		glm::mat4 camera = glm::lookAt(glm::vec3(0, 0, 0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0));
-		glm::mat4 perspective = glm::perspective(glm::radians(45.0), static_cast<double>(window.getSize().x) / window.getSize().y, 0.1, 100.0);
+		glm::mat4 camera{ 
+			glm::lookAt(glm::vec3(0, 0, 0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0)) }
+		;
+		glm::mat4 perspective{
+			glm::perspective(glm::radians(45.0), static_cast<double>(window.getSize().x) / window.getSize().y, 0.1, 100.0)
+		};
 		program.setUniform("view", camera);
 		program.setUniform("projection", perspective);
 
 		// Adjust object position and rebuild model matrix.
-		glm::mat4 model = buildModelMatrix(objectPosition, objectOrientation, objectScale);
+		glm::mat4 model{
+			buildModelMatrix(objectPosition, objectOrientation, objectScale)
+		};
 		program.setUniform("model", model);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
